6aee2867c9a8f814879c001b81b579c0ee843202
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-31 10:34:15 +0100

    Bottom bar routing and navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,53 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { View, Text } from 'react-native'
+import { Tabs } from 'expo-router'
+import {Ionicons} from "@expo/vector-icons"
+import { HomeIcon } from "@/components/icons/HomeIcon";
+import { HomeIconActive } from "@/components/icons/HomeIconActive";
+// import {COLORS} from "@/constants/theme"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarStyle:{
+                backgroundColor: "rgba(255, 255, 255, 0.95)",
+                borderTopWidth: 0,
+                position: "absolute",
+                elevation: 0,
+                height: 90,
+                paddingBottom: 8,
+            }
+            // tabBarActiveTintColor: Colors.primary
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    >
+
+        <Tabs.Screen 
+            name="HomePage"
+            options={{
+                tabBarIcon: ({size, color}) => <Ionicons name="home" size={size} color={color}/>
+            }}
+        />
+
+        <Tabs.Screen 
+            name="Cart"
+            options={{
+                tabBarIcon: ({size, color}) => <Ionicons name="add-outline" size={size} color={color}/>
+            }}
+        />
+        <Tabs.Screen 
+            name="Favorites"
+            options={{
+                tabBarIcon: ({size, color}) => <Ionicons name="chatbox-ellipses-outline" size={size} color={color}/>
+            }}
+        />
+        <Tabs.Screen 
+            name="Profile"
+            options={{
+                tabBarIcon: ({size, color}) => <Ionicons name="person-circle-outline" size={size} color={color}/>
+            }}
+        />
     </Tabs>
-  );
+  )
 }
